6215b003d4f33b95e8d56cc0348720ac4b2b743b
mijan44 <82171609+mijan44@users.noreply.github.com>
2022-10-30 14:55:56 +0800

    Add project files.

---
+++ BulkyBook/Migrations/20221023083416_AddCategoryToDatebase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToDatebase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
